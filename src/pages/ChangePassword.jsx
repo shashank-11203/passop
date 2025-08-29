1519ecb048bc67e7cdd475bd46e60e66372247f3
shashank410 <shashanksolanki020@gmail.com>
2025-08-29 21:27:35 +0530

    padding

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -34,11 +34,11 @@ const ChangePassword = () => {
     return (
         <>
             <ToastContainer closeOnClick />
-            <div className="relative isolate bg-gray-900 px-6 py-20 sm:py-32 lg:px-8">
+            <div className="relative isolate bg-gray-900 px-6 py-12 sm:py-32 lg:px-8">
                 <div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
                     <div style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }} className="mx-auto aspect-1155/678 w-288.75 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20"></div>
                 </div>
-                <div className="min-h-screen flex items-center justify-center x-4 my-2">
+                <div className="min-h-screen flex items-center justify-center x-4 my-1">
                     <form onSubmit={handleReset} className="max-w-md w-full p-6 rounded shadow-md">
                         <h2 className="text-2xl font-bold mb-4 text-white">Change Your Password</h2>
                         <input
